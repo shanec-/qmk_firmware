8179d022d89906f813f26601428e3d7f9fe3cbff
shane carvalho <shanec_@hotmail.com>
2021-12-19 13:32:04 +1100

    Fine tune encode resolution

--- keyboards/sofle/keymaps/nullfactory/config.h
+++ keyboards/sofle/keymaps/nullfactory/config.h
@@ -4,3 +4,12 @@
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
+
+#ifdef ENCODER_RESOLUTION
+#undef ENCODER_RESOLUTION
+#endif
+
+#ifdef ENCODER_RESOLUTIONS
+#undef ENCODER_RESOLUTIONS
+#define ENCODER_RESOLUTIONS { 4, 6 }
+#endif
